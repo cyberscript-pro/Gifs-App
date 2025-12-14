f0506ada45feae937a8f7419b72e1f008b5369e3
Li Anthony Dev <li.3108gomezgonzalez@gmail.com>
2025-12-14 10:01:30 -0500

    feat: implement custom hook

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,35 +1,11 @@
-import { useState } from "react";
 import { CustomHeader } from "./common/components/CustomHeader";
 import { SearchBar } from "./common/components/SearchBar";
 import PreviousSearches from "./gifs/components/PreviousSearches";
-import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
 import GifsList from "./gifs/components/GifsList";
-import type { Gif } from "./gifs/interfaces/gif.interface";
+import useGifs from "./gifs/hooks/useGifs";
 
 const GifsApp = () => {
-  const [previousTerms, setPreviosTerms] = useState<string[]>([]);
-  const [gifs, setGifs] = useState<Gif[]>([]);
-
-  const handleTermClicked = (term: string) => {
-    console.log(term);
-  };
-
-  const handleSearch = async (query: string) => {
-    if (query.length === 0) return;
-
-    const queryConv = query.trim().toLowerCase();
-
-    if (previousTerms.includes(query)) return;
-
-    if (previousTerms.length >= 8) {
-      previousTerms.pop();
-    }
-
-    setPreviosTerms([queryConv, ...previousTerms]);
-
-    const data = await getGifsByQuery(queryConv);
-    setGifs(data);
-  };
+  const { gifs, previousTerms, handleTermClicked, handleSearch } = useGifs();
 
   return (
     <div className="p-1 min-h-screen bg-[#242424] text-white">
---
+++ src/gifs/hooks/useGifs.tsx
@@ -0,0 +1,50 @@
+import { useRef, useState } from "react";
+import type { Gif } from "../interfaces/gif.interface";
+import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
+
+const useGifs = () => {
+  const [previousTerms, setPreviosTerms] = useState<string[]>([]);
+  const [gifs, setGifs] = useState<Gif[]>([]);
+  const gifsCache = useRef<Record<string, Gif[]>>({});
+
+  const handleTermClicked = async (term: string) => {
+    if (gifsCache.current[term]) {
+      setGifs(gifsCache.current[term]);
+      return;
+    }
+
+    const data = await getGifsByQuery(term);
+    setGifs(data);
+  };
+
+  const handleSearch = async (query: string) => {
+    if (query.length === 0) return;
+
+    const queryConv = query.trim().toLowerCase();
+
+    if (previousTerms.includes(query)) return;
+
+    if (previousTerms.length >= 8) {
+      previousTerms.pop();
+    }
+
+    setPreviosTerms([queryConv, ...previousTerms]);
+
+    const data = await getGifsByQuery(queryConv);
+    setGifs(data);
+
+    gifsCache.current[queryConv] = data;
+  };
+
+  return {
+    //Values
+    gifs,
+    previousTerms,
+
+    //Methods
+    handleTermClicked,
+    handleSearch,
+  };
+};
+
+export default useGifs;
